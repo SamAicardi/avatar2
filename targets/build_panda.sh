ea023abd2129c1e77522bef1f993005552b9b924
SamAicardi <samuele.aicardi@eurecom.fr>
2018-02-19 09:44:38 +0100

    Minor modification to the first part of the script

--- targets/build_panda.sh
+++ targets/build_panda.sh
@@ -5,21 +5,19 @@ ci_distr="Ubuntu 16.04.3 LTS \n \l"
 
 # Usage: ./build_panda.sh [build-llvm | build-get-llvm [enable-llvm-debug]] 
 
-if [[ 0 -eq 0 ]] ; then
-    if [[ "$distr" == "$ci_distr" ]]
-    then
-      echo "deb-src http://archive.ubuntu.com/ubuntu/ xenial-security main restricted" >> /etc/apt/sources.list
-      apt-get update
-      apt-get build-dep -y qemu
+if [[ "$distr" == "$ci_distr" ]]
+then
+  echo "deb-src http://archive.ubuntu.com/ubuntu/ xenial-security main restricted" >> /etc/apt/sources.list
+  apt-get update
+  apt-get build-dep -y qemu
 
-      # panda-specific deps below, taken from panda/scripts/install_ubuntu.sh
-      apt-get -y install python-pip git protobuf-compiler protobuf-c-compiler \
-          libprotobuf-c0-dev libprotoc-dev libelf-dev libc++-dev pkg-config
-      apt-get -y install software-properties-common
-      add-apt-repository -y ppa:phulin/panda
-      apt-get update
-      apt-get -y install libcapstone-dev libdwarf-dev python-pycparser
-    fi
+  # panda-specific deps below, taken from panda/scripts/install_ubuntu.sh
+  apt-get -y install python-pip git protobuf-compiler protobuf-c-compiler \
+      libprotobuf-c0-dev libprotoc-dev libelf-dev libc++-dev pkg-config
+  apt-get -y install software-properties-common
+  add-apt-repository -y ppa:phulin/panda
+  apt-get update
+  apt-get -y install libcapstone-dev libdwarf-dev python-pycparser
 fi
 
 cd `dirname "$BASH_SOURCE"`/src/
