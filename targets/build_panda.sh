bb17a9039b949c738d339bb6b590c6fe5ea7420a
SamAicardi <samuele.aicardi@eurecom.fr>
2018-02-20 15:05:48 +0100

    Clean up the script to run in vagrantw

--- targets/build_panda.sh
+++ targets/build_panda.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+#==========================================================================
+# Usage: ./build_panda.sh [build-llvm | build-get-llvm [enable-llvm-debug]] 
+#==========================================================================
+
 distr=`cat /etc/issue`
 ci_distr="Ubuntu 16.04.3 LTS \n \l"
 
-
-# Usage: ./build_panda.sh [build-llvm | build-get-llvm [enable-llvm-debug]] 
-
+#============================================
+#           Install dependencies
+#============================================
+echo -e "\e[1m[$(basename $0)] Installing dependencies ...\e[0m"
 if [[ "$distr" == "$ci_distr" ]]
 then
   sudo sh -c 'echo "deb-src http://archive.ubuntu.com/ubuntu/ xenial-security main restricted" >> /etc/apt/sources.list'
@@ -17,24 +22,40 @@ then
   sudo apt-get -y install software-properties-common
   sudo add-apt-repository -y ppa:phulin/panda
 
+  sudo apt-get install subversion
   sudo apt-get -y install libcapstone-dev libdwarf-dev python-pycparser
 fi
 
+
+#============================================
+#           Install submodules
+#============================================
+echo -e "\e[1m[$(basename $0)] Installing submodules ...\e[0m"
 cd `dirname "$BASH_SOURCE"`/src/
 git submodule update --init avatar-panda
 
 cd avatar-panda
 git submodule update --init dtc
 
+
+#============================================
+#                    Misc
+#============================================
 mkdir -p ../../build/panda/panda
 
+# TODO: check if it is required
 sudo chown -R vagrant:vagrant ../../ # i.e. /home/vagrant/avatar2/targets
 
+
+#============================================
+#         Download or prepare LLVM
+#============================================
 # here I am in src/avatar-panda
-llvm=0
+pushd .
+build_llvm=0
 if [[ $1 == "build-get-llvm" ]] ; then 
-    llvm=1
-    #cd ../../build/panda/panda
+    echo -e "\e[1m[$(basename $0)] Downloading LLVM ...\e[0m"
+    build_llvm=1
     cd panda
     svn checkout http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_33/final/ llvm
     cd llvm/tools
@@ -42,40 +63,47 @@ if [[ $1 == "build-get-llvm" ]] ; then
     cd -
     cd llvm/tools/clang/tools
     svn checkout http://llvm.org/svn/llvm-project/clang-tools-extra/tags/RELEASE_33/final/ extra
-    cd -
-    cd llvm
-    if [[ $2 == "enable-llvm-debug" ]] ; then 
-        ./configure --disable-optimized --enable-debug-runtime --enable-targets=host,arm && REQUIRES_RTTI=1 make -j $(nproc)
-    else
-        ./configure --enable-optimized --disable-assertions --enable-targets=host,arm && REQUIRES_RTTI=1 make -j $(nproc)
-    fi
-    # install llvm3.3 under /usr/local/
-    sudo make install
-    cd ../../../src/avatar-panda
 
 else
     if [[ $1 == "build-llvm" ]] ; then 
-        llvm=1
-        # configure LLVM
-        #cd ../../build/panda/panda/llvm
+        echo -ne "\e[1m[$(basename $0)] LLVM sub-tree already downloaded. Cleaning ... \e[0m"
+        build_llvm=1
         cd panda/llvm
         make clean
-        if [[ $2 == "enable-llvm-debug" ]] ; then 
-            ./configure --disable-optimized --enable-debug-runtime --enable-targets=host,arm && REQUIRES_RTTI=1 make -j $(nproc)
-        else
-            ./configure --enable-optimized --disable-assertions --enable-targets=host,arm && REQUIRES_RTTI=1 make -j $(nproc)
-        fi
-        # install llvm3.3 under /usr/local/
-        sudo make install
-        cd -
     fi
 fi
+popd # src/avatar-panda
 
-# it seems the flows does not reach this part
+
+#============================================
+#               Install LLVM
+#============================================
+if [[ $build_llvm -eq 1 ]] ; then
+    pushd .
+    cd panda/llvm
+    if [[ $2 == "enable-llvm-debug" ]] ; then 
+        echo -e "\e[1m[$(basename $0)] Configuring and making LLVM in debug mode ...\e[0m"
+        ./configure --disable-optimized --enable-debug-runtime --enable-targets=host,arm && REQUIRES_RTTI=1 make -j $(nproc)
+    else
+        echo -e "\e[1m[$(basename $0)] Configuring and making LLVM in release mode ...\e[0m"
+        ./configure --enable-optimized --disable-assertions --enable-targets=host,arm && REQUIRES_RTTI=1 make -j $(nproc)
+    fi
+    # install llvm3.3 under /usr/local/
+    echo -e "\e[1m[$(basename $0)] Installing LLVM in /usr/local/ ...\e[0m"
+    sudo make install
+    popd
+fi
+
+
+#============================================
+#             Install PANDA
+#============================================
 cd ../../build/panda/panda
 if [[ $llvm -eq 1 ]] ; then  
+    echo -ne "\e[1m[$(basename $0)] Installing PANDA with LLVM support ...\e[0m"
     ../../../src/avatar-panda/configure --disable-sdl --target-list=arm-softmmu --enable-llvm --with-llvm=/usr/local
 else
+    echo -ne "\e[1m[$(basename $0)] Installing PANDA (no LLVM support) ...\e[0m"
     ../../../src/avatar-panda/configure --disable-sdl --target-list=arm-softmmu
 fi
 make -j4
